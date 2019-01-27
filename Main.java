f4c91ad111d2065a4172de9495c5d99e1b277ffd
JCVelazco <juan.carlosvr.97@hotmail.com>
2019-01-27 00:13:46 -0600

    add simple house

--- Main.java
+++ Main.java
@@ -6,8 +6,8 @@ public class Main extends JPanel{
 	
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
-		//sky
-		float red = 0f, green = 1f, blue = 1f;
+		//gradient sky
+		float red = 0f, green = 1f, blue = 1f; //aqua
 		for(int r = 0; r < 1000; r ++){
 			for(int c = 0; c < 1950; c++){
 				g.setColor(new Color(red, green, blue));
@@ -15,18 +15,18 @@ public class Main extends JPanel{
 			}	
 			red = red + 0.001f;		
 		}
-		//grass
-		red = 0f; green = 0.8f; blue = 0f;
+		// simple grass
+		red = 0f; green = 0.8f; blue = 0f; //1/2 black green
 		for(int r = 800; r < 1000; r ++){
 			for(int c = 0; c < 1950; c++){
 				g.setColor(new Color(red, green, blue));
 				g.drawLine(c, r, c, r);	
 			}	
 		}
-		//sun
-		red = 1f; green = 0.4f; blue = 0f;
-		double xOfSun = 1400;
-		double yOfSun = 100;
+		// gradient sun
+		red = 1f; green = 0.4f; blue = 0f; //orange
+		double xOfSun = 1500;
+		double yOfSun = 80;
 		for(int depth = 300; depth > 0; depth--){
 			g.setColor(new Color(red,green,blue));
 			g.fillOval((int)xOfSun, (int)yOfSun, depth, depth);
@@ -34,7 +34,19 @@ public class Main extends JPanel{
 			yOfSun =  yOfSun + 0.5;
 			green = green + 0.002f;
 		}
-
+		
+		//gradient house
+		//house:
+		red = 1f; green = 0.85f; blue = 0.7f; //light brown
+		for(int r = 400; r < 850; r ++){
+			for(int c = 50; c < 450; c++){
+				g.setColor(new Color(red, green, blue));
+				g.drawLine(c, r, c, r);	
+			}		
+			blue =  blue + 0.0005f;
+		}
+		
+		
 	}
 	
 	public static void main(String args[]){
