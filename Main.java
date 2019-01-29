414d5c284c97d4456814f83380e4b4ad89e03521
Carlos Parrodi <carlosparrodimartinez@hotmail.com>
2019-01-28 23:37:46 -0600

    se agrego el carrito fase 1

--- Main.java
+++ Main.java
@@ -88,9 +88,32 @@ public class Main extends JPanel{
 		int [] xPointsRoof = new int[] {50,450,250};
 		int [] yPointsRoof = new int[] {500, 500, 400};
 		g.setColor(new Color(red, green, blue));
-		g.fillPolygon(xPointsRoof, yPointsRoof, 3);	
+		g.fillPolygon(xPointsRoof, yPointsRoof, 3);
 		
+		g.setColor(new Color(255,0,0));
+		int[] xPointsBocho =  {683,665,645,631,614,605,595,584,576,571,566,563,566,559,558,550,550,544,544,552,552,562,562,572,578,582,590,604,615,625,632,639,643,648,660,663,780,780,788,790,792,795,800,808,818,831,842,848,854,860,868,877,890,895,895,906,906,900,900,894,888,885,881,877,881,880,874,868,856,848,816,797,784,770,716};
+		int[] yPointsBocho =  {767,801,802,805,809,813,817,824,833,840,854,852,860,863,858,858,851,857,881,881,878,878,887,875,869,860,848,840,840,844,851,863,878,888,888,883,883,890,890,883,871,860,853,844,841,841,847,855,865,876,882,884,886,886,880,880,860,857,852,858,858,853,845,838,838,832,832,820,805,794,769,759,755,754,755};
+		g.fillPolygon(xPointsBocho, yPointsBocho, 75);
+
+		g.setColor(new Color(0,0,0));
+		g.fillOval(581, 842, 58, 59);
+		g.fillOval(794, 842, 58, 59);
+		g.setColor(new Color(255,255,255));
+		g.fillOval(809, 857, 29, 30);
+		g.setColor(new Color(0,0,0));
+		g.fillOval(810, 858, 27, 28);
+		g.setColor(new Color(255,255,255));
+		g.fillOval(814, 862, 19, 20);
+		
+		g.fillOval(595, 857, 29, 30);
+		g.setColor(new Color(0,0,0));
+		g.fillOval(596, 858, 27, 28);
+		g.setColor(new Color(255,255,255));
+		g.fillOval(600, 862, 19, 20);
+		
+
 	}
+
 	
 	public static void main(String args[]){
 		Main panel = new Main();
