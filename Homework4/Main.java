aabc2e07f0d266a3eac913dc275fa94670966ba6
JCVelazco <juan.carlosvr.97@hotmail.com>
2019-02-03 17:46:13 -0600

    clean the code structure

--- Homework4/Main.java
+++ Homework4/Main.java
@@ -5,8 +5,6 @@ import javax.swing.JFrame;
 import java.lang.Math;
 import java.util.Scanner;
 import java.io.File; 
-import java.util.Random;
-
 public class Main extends JPanel{
     
     //we make them public variables because they will be used in the paintComponent
@@ -16,14 +14,17 @@ public class Main extends JPanel{
     
     //colors
     public static float red, green, blue;
+
+    //angles
+    public static double angle;
+    public static double anguloFinal;
     
 	public void paintComponent(Graphics g){
         super.paintComponent(g);
-        paintPieChart(g);
-        paintBarGraph(g);  
+        paintGraphs(g); 
     }
     
-    public static void paintPieChart(Graphics g){
+    public static void paintGraphs(Graphics g){
         double sum = 0;
         
         for(int i = 0 ; i < size ; i++)
@@ -33,24 +34,27 @@ public class Main extends JPanel{
         for(int i =0; i < size; i++){
             arrPercentages[i] = (values[i]*100)/sum;
         }
-        
-        double angle = 0;
-        double anguloFinal = 0;
-        double anguloApetura;
+    
         
         for(int i = 0; i < size; i++){
             randomizeColors();
             g.setColor(new Color(red,green,blue));
-            //calculo angulo
-            anguloApetura = ((arrPercentages[i]*360.00)/100.00);
-            anguloFinal = angle+(arrPercentages[i]*360.00)/100.00;
-            g.fillArc(100, 100, 360, 360, (int)angle, (int)anguloApetura);
+            paintPieGraph(g, arrPercentages, i);
+            paintBarGraph(g, arrPercentages, i);
             paintSquares(g, i);
             paintPercentages(g, angle, anguloFinal, arrPercentages, i);
             angle = anguloFinal;
         }
         
     }
+
+    public static void paintPieGraph(Graphics g, double [] arrPercentages, int nElement){
+        double anguloApetura = ((arrPercentages[nElement]*360.00)/100.00);
+        anguloFinal = angle+(arrPercentages[nElement]*360.00)/100.00;
+        g.fillArc(100, 100, 360, 360, (int)angle, (int)anguloApetura);
+        angle = anguloFinal;
+    }
+
     
     public static void paintBarGraph(Graphics g){
         
@@ -72,7 +76,6 @@ public class Main extends JPanel{
         g.setColor(new Color(1f,1f,1f));
         //check the 4 "cuadrantes"
         double middleAngle = (initialAngle+finalAngle)/2;
-        System.out.println(initialAngle+" "+ finalAngle+" : "+middleAngle);//nice
 
         if(middleAngle >= 0 && middleAngle <= 90){
             //y-- and x++
@@ -84,6 +87,8 @@ public class Main extends JPanel{
             //y-- and x--
             //normalize the angle to be from 0 - 90
             middleAngle -= 90;
+            middleAngle = 90 - middleAngle;
+
             int yMove = (int)middleAngle;
             int xMove = 180 - yMove;
             g.drawString(myPercentage, 280-xMove, 280-yMove);
