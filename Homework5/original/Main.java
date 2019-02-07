57b8c2d491d9ca7f067a7fd89128532c7ee0a55c
JCVelazco <juan.carlosvr.97@hotmail.com>
2019-02-06 18:04:24 -0600

    add basic methods in the main

--- Homework5/original/Main.java
+++ Homework5/original/Main.java
@@ -6,51 +6,11 @@ import javax.swing.JPanel;
 import javax.swing.JFrame;
 import java.lang.Math;
 
-/*
-Computer Graphics
-
-In-Class Assignment 
-
-Create methods in a graphical application in Java that "draw vector based instructions"
-
-The methods to be implemented are:
-
-Void position (int x, int y)
-Void goFowardDrawing (double pixels)
-Goes foward and draws a line using the last specified color
-Void goFoward (double pixels)
-Goes foward without drawing
-Void rotate (double angle)
-It only changes the direction where the thing that draws "points"
-
-The starting angle of the application is 0 degrees, so to paint a square, you could call the following instructions:
-
-position (200, 300);
-goFowardDrawing (g,100);
-rotate (90);
-goFowardDrawing (g,100);
-rotate (90);
-goFowardDrawing (g,100);
-rotate (90);
-goFowardDrawing (g,100);
-rotate (90);
-
-Once you have the methods working, draw the house on page 9 of the Concepts document
-(TIP: It may be a good idea for the methods to receive a reference to the "g" object of the Graphics class as a parameter)
-the current position can be implemented as double class variables
-
-Upload a ZIP file containing 4 folders with a Main.java file each one
-. Folder "original" where the house is painted originally as it is defined
-. Folder "a" where the first operation mentioned in page 20 of the document "Concepts" are applied.
-. Folder "b" where the second operation mentioned in page 20 of the document "Concepts" are applied.
-. Folder "c" where the third operation mentioned in page 20 of the document "Concepts" are applied.
-*/
-
 public class Main extends JPanel{
 
 	public static double generalAngle = 0;
-	public static int generalX = 0;
-	public static int generalY = 0;
+	public static double generalX = 0;
+	public static double generalY = 0;
 
 	
 	public void paintComponent(Graphics g){
@@ -69,18 +29,31 @@ public class Main extends JPanel{
 	public void position (int x, int y){
 		generalX = x;
 		generalY = y;
+		System.out.println("My new x is: "+generalX+" and my new Y is: "+generalY);
 	}
 	//Goes foward and draws a line using the last specified color
 	public void goFowardDrawing (Graphics g, double pixels){
-		g.drawLine(c, r, c, r);	
+		double finalX = pixels*(Math.cos(generalAngle));
+		double finalY = pixels*(Math.sin(generalAngle));
+		g.drawLine((int)generalX, (int)generalY, (int)finalX, (int)finalY);	
+		generalX = finalX;
+		generalY = finalY;
+		System.out.println("My new x is: "+generalX+" and my new Y is: "+generalY);
 	}
 	//Goes foward without drawing
 	public void goFoward (Graphics g, double pixels){
-
+		generalX = pixels*(Math.cos(generalAngle));
+		generalY = pixels*(Math.sin(generalAngle));
 	}
 	//It only changes the direction where the thing that draws "points"
 	public void rotate (double angle){
+		if((generalAngle+angle)>360){
+			double inbetweenAngle = 360 - generalAngle;
+			generalAngle = angle - inbetweenAngle;
+		}else{
 		generalAngle += angle;
+		}
+		System.out.println("My new angle is: "+generalAngle);
 	}
 	
 	
