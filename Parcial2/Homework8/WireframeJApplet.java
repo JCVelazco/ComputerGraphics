a1933ab93b0d1ec4f712dead21fae6dc28f768c5
JCVelazco <juan.carlosvr.97@hotmail.com>
2019-03-10 16:55:02 -0600

    finish the colors for landscape

--- Parcial2/Homework8/WireframeJApplet.java
+++ Parcial2/Homework8/WireframeJApplet.java
@@ -18,23 +18,24 @@ class Point3D {
 
 class Edge {
    public int a, b;
-   public Edge( int A, int B ) {
-      a = A;  b = B;
+   public Color mycolor;
+   public Edge( int A, int B, Color myColor ) {
+      a = A;  b = B; this.mycolor = myColor;
    }
 }
 
 public class WireframeJApplet extends JApplet 
-                  implements KeyListener, FocusListener, MouseListener {
-                      
+implements KeyListener, FocusListener, MouseListener {
+   
    int width, height;
    // int mx, my;  // the most recently recorded mouse coordinates
-
+   
    int azimuth = 35, elevation = 30;
    //rotation y, and x
    
    ArrayList<Point3D> vertices;
    ArrayList<Edge> edges;
-
+   
    boolean focussed = false;   // True when this applet has input focus.
    
    DisplayPanel canvas;
@@ -42,9 +43,9 @@ public class WireframeJApplet extends JApplet
    double xSize = 6;
    double ySize = 1;
    double zSize = 6;
-
+   
    public void init() {
-
+      
       //Primeros cuatro puntos del plano 2x2
       Point3D a = new Point3D( -xSize/2, getRandom(-ySize, ySize), zSize/2 ); 
       Point3D b = new Point3D( -xSize/2, getRandom(-ySize, ySize), -zSize/2 );
@@ -53,39 +54,39 @@ public class WireframeJApplet extends JApplet
       
       vertices = new ArrayList<Point3D>(); 
       edges = new ArrayList<Edge>();
-
+      
       Point3D[][] matrix = {{a,d},
-                            {b,c}};
-
+      {b,c}};
+      
       int iterations = 6;
       //Aquí comienza el procedimiento of creating the new matrix
       Point3D[][] finalMatrix = squaredDiamond(matrix, iterations);
       //Una vez teniendo la matriz completa, se manda a formato vertices/edges
       setEdges(finalMatrix);
-
+      
       canvas = new DisplayPanel();  // Create drawing surface and 
       setContentPane(canvas);       //    install it as the applet's content pane.
-   
+      
       canvas.addFocusListener(this);   // Set up the applet to listen for events
       canvas.addKeyListener(this);     //                          from the canvas.
       canvas.addMouseListener(this);
       
    } // end init();
-
+   
    //Este método controla los pasos del procedimiento
    public Point3D[][] squaredDiamond(Point3D[][] matrix, int iterations){
       Point3D[][] actualMatrix = matrix;
       Point3D[][] newMatrix;
-
+      
       for(int i=0; i<iterations; i++){
-      newMatrix = placeInitialValues(actualMatrix); 
-      newMatrix = getCenters(newMatrix,i+1);
-      newMatrix = getAdyacent(newMatrix,i+1);
-      actualMatrix = newMatrix;
+         newMatrix = placeInitialValues(actualMatrix); 
+         newMatrix = getCenters(newMatrix,i+1);
+         newMatrix = getAdyacent(newMatrix,i+1);
+         actualMatrix = newMatrix;
       }
       return actualMatrix;
    }
-
+   
    //Se crea matriz 2*n-1 y se ingresan los valores de la matriz pasada en sus respectivos lugares
    public Point3D[][] placeInitialValues(Point3D[][] actualMatrix){
       Point3D[][] newMatrix = new Point3D[(actualMatrix[0].length*2)-1][(actualMatrix[0].length*2)-1];
@@ -111,10 +112,10 @@ public class WireframeJApplet extends JApplet
             matrix[newi][newj] = new Point3D(averagex,averagey,averagez); 
          }
       }
-
+      
       return matrix;
    }
-
+   
    //Se calculan todos los espacios adyacentes restantes
    public Point3D[][] getAdyacent(Point3D[][] matrix, int times){
       double x,averagey,z;
@@ -127,22 +128,22 @@ public class WireframeJApplet extends JApplet
                   x = matrix[i+1][j].x;
                   z = matrix[i][j+1].z;
                   averagey = (matrix[i][j+1].y+matrix[i][j-1].y+matrix[i+1][j].y)/3;
-               //if i am in the first col (i can't j-1)
+                  //if i am in the first col (i can't j-1)
                }else if(j==0){
                   x = matrix[i+1][j].x;
                   z = matrix[i][j+1].z;
                   averagey = (matrix[i][j+1].y+matrix[i-1][j].y+matrix[i+1][j].y)/3;
-               //if i am in the last row (i can't i+1)
+                  //if i am in the last row (i can't i+1)
                }else if(i==matrix[0].length-1){
                   x = matrix[i-1][j].x;
                   z = matrix[i][j+1].z;
                   averagey = (matrix[i][j+1].y+matrix[i][j-1].y+matrix[i-1][j].y)/3;
-               //if i am in the last col (i can't j+1)
+                  //if i am in the last col (i can't j+1)
                }else if(j==matrix[0].length-1){
                   x = matrix[i+1][j].x;
                   z = matrix[i][j-1].z;
                   averagey = (matrix[i][j-1].y+matrix[i-1][j].y+matrix[i+1][j].y)/3;
-               //middle cases
+                  //middle cases
                }else{
                   x = matrix[i+1][j].x;
                   z = matrix[i][j+1].z;
@@ -174,19 +175,34 @@ public class WireframeJApplet extends JApplet
                Point3D rightPoint = matrix[i][j+1];
                vertices.add(rightPoint);
                int rightindex= vertices.indexOf(rightPoint);
-               edges.add(new Edge(actualIndex, rightindex));
+               edges.add(new Edge(actualIndex, rightindex, defineColor(actualIndex, rightindex)));
             }
-            //try to connect with my right side:
+            //try to connect with my right side
             if(i+1 < matrix[0].length ){
                Point3D downPoint = matrix[i+1][j];
                vertices.add(downPoint);
                int downIndex= vertices.indexOf(downPoint);
-               edges.add(new Edge(actualIndex, downIndex));
+               edges.add(new Edge(actualIndex, downIndex, defineColor(actualIndex, downIndex)));
             }
          }
       }
    }
+   //this method will recieve the index of two points in the array of vertices, and in order of this "edge"
+   //i will return a color: very negative -> blue, near 0 brown and very positive -> green
+   public Color defineColor(int firstIndex, int secondIndex){
+      Point3D firstP = vertices.get(firstIndex);
+      Point3D secondP = vertices.get(secondIndex);
+      double promedioY = (firstP.y + secondP.y)/2;
 
+      if(promedioY < -0.5){
+         return Color.BLUE;
+      }else if(promedioY > 0.5){
+         return Color.GREEN;
+      }else{
+         return new Color(100,65,30);
+      }
+   }
+   
    //Método para debugear que imprime la matriz
    public void printMatrix(Point3D[][] matrix){
       for(int i=0; i<matrix[0].length;i++){
@@ -196,27 +212,27 @@ public class WireframeJApplet extends JApplet
          System.out.println();
       }
    }
-
+   
    //Devuelve un valor random
    public static double getRandom(double min, double max){
-		return ThreadLocalRandom.current().nextDouble(min, max);
+      return ThreadLocalRandom.current().nextDouble(min, max);
    } 
-
+   
    
    class DisplayPanel extends JPanel {
       public void paintComponent(Graphics g) {
          super.paintComponent(g);  
          if (focussed) 
-            g.setColor(Color.cyan);
+         g.setColor(Color.cyan);
          else
-            g.setColor(Color.lightGray);
-
+         g.setColor(Color.lightGray);
+         
          int width = getSize().width;  // Width of the applet.
          int height = getSize().height; // Height of the applet.
          g.drawRect(0,0,width-1,height-1);
          g.drawRect(1,1,width-3,height-3);
          g.drawRect(2,2,width-5,height-5);
-
+         
          if (!focussed) {
             g.setColor(Color.magenta);
             g.drawString("Click to activate",100,120);
@@ -231,8 +247,8 @@ public class WireframeJApplet extends JApplet
             float cosP = (float)Math.cos( phi ), sinP = (float)Math.sin( phi );
             
             float cosTcosP = cosT*cosP, cosTsinP = cosT*sinP,
-                  sinTcosP = sinT*cosP, sinTsinP = sinT*sinP;
-
+            sinTcosP = sinT*cosP, sinTsinP = sinT*sinP;
+            
             // project vertices onto the 2D viewport
             Point[] points;
             points = new Point[ vertices.size() ];
@@ -240,55 +256,47 @@ public class WireframeJApplet extends JApplet
             int scaleFactor = width/8;
             float near = 3;  // distance from eye to near plane
             float nearToObj = 3f;  // distance from near plane to center of object
-
+            
             //point3D -> point2D
             for ( j = 0; j < vertices.size(); ++j ) {
                double x0 = vertices.get(j).x;
                double y0 = vertices.get(j).y;
                double z0 = vertices.get(j).z;
-
+               
                // compute an orthographic projection
                double x1 = cosT*x0 + sinT*z0;
                double y1 = -sinTsinP*x0 + cosP*y0 + cosTsinP*z0;
                double z1 = cosTcosP*z0 - sinTcosP*x0 - sinP*y0;
-
+               
                // now adjust things to get a perspective projection
                x1 = x1*near/(z1+near+nearToObj);
                y1 = y1*near/(z1+near+nearToObj);
-
+               
                // the 0.5 is to round off when converting to int
                points[j] = new Point(
-                  (int)(width/2 + scaleFactor*x1 + 0.5),
-                  (int)(height/2 - scaleFactor*y1 + 0.5)
+               (int)(width/2 + scaleFactor*x1 + 0.5),
+               (int)(height/2 - scaleFactor*y1 + 0.5)
                );
             }
-
+            
             // draw the wireframe
             g.setColor( Color.black );
             g.fillRect( 0, 0, width, height );
             g.setColor( Color.white );
             
-
+            
             for ( j = 0; j < edges.size(); ++j ) {
-
-               //Aquí se calculan los colores dependiendo de la altura en 'y', tomando en cuenta que el centro de 'y' es 400
-               /*if((points[ (edges.get(j).a) ].y + points[ (edges.get(j).b) ].y)/2 < 350)
-               g.setColor( Color.GREEN );
-            else if((points[ (edges.get(j).a) ].y + points[ (edges.get(j).b) ].y)/2 > 450)
-               g.setColor( Color.BLUE );
-            else 
-               g.setColor(new Color(101, 67, 33));*/
-
+               g.setColor(edges.get(j).mycolor);
                g.drawLine(
-                  points[ (edges.get(j).a) ].x, points[ (edges.get(j).a) ].y,
-                  points[ (edges.get(j).b) ].x, points[ (edges.get(j).b) ].y
+               points[ (edges.get(j).a) ].x, points[ (edges.get(j).a) ].y,
+               points[ (edges.get(j).b) ].x, points[ (edges.get(j).b) ].y
                );
             }
-
+            
          } 
       }  // end paintComponent()    
-    } // end nested class DisplayPanel 
-
+   } // end nested class DisplayPanel 
+   
    // ------------------- Event handling methods ----------------------
    
    public void focusGained(FocusEvent evt) {
@@ -300,12 +308,12 @@ public class WireframeJApplet extends JApplet
       focussed = false;
       canvas.repaint(); 
    }
-      
+   
    public void keyTyped(KeyEvent evt) {  
    }  // end keyTyped()
-      
+   
    public void keyPressed(KeyEvent evt) { 
-       
+      
       int key = evt.getKeyCode();  // keyboard code for the key that was pressed
       
       if (key == KeyEvent.VK_LEFT) {
@@ -324,9 +332,9 @@ public class WireframeJApplet extends JApplet
          elevation += 5;         
          canvas.repaint();
       }
-
+      
    }  // end keyPressed()
-
+   
    public void keyReleased(KeyEvent evt) { 
       // empty method, required by the KeyListener Interface
    }
